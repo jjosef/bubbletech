ef54c9ed7aa0bf4d73b1b666af5b18f130dff0e8
John Josef <john.josef@gmail.com>
2021-04-19 13:16:23 -0400

    cleanup

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 import { config } from './index.js';
 
 if (!mongoose.connection.readyState) {
-  mongoose.connect(config.MONGOOSE_CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect(config.MONGOOSE_CONNECT_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 const db = mongoose.connection;
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 const config = {
-  MONGOOSE_CONNECT_URL: process.env.MONGOOSE_CONNECT_URL || 'mongodb://root:password@localhost'
-}
+  MONGOOSE_CONNECT_URL:
+    process.env.MONGOOSE_CONNECT_URL || 'mongodb://root:password@localhost',
+};
 
 export { config };
